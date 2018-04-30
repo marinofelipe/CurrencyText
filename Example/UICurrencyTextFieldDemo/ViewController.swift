b9e144a677584e7940f1c44063b9e57334a48e6b
Felipe Lefèvre Marino <felipemarino91@gmail.com>
2018-04-29 23:38:05 -0300

    feat: add programmatically inited text field

--- Example/UICurrencyTextFieldDemo/ViewController.swift
+++ Example/UICurrencyTextFieldDemo/ViewController.swift
@@ -12,11 +12,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var textField: UICurrencyTextField!
+    @IBOutlet weak var programaticallyAddedLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configCurrencyTextField()
+        configTextFieldOutlet()
+        programaticallyAddTextField()
     }
 
     
@@ -24,8 +26,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-    private func configCurrencyTextField() {
+    private func configTextFieldOutlet() {
         textField.maximumIntegers = 4
         textField.hasAutoclear = true
+    }
+    
+    private func programaticallyAddTextField() {
+        let textFieldFrame = CGRect(x: view.frame.width / 2 - 87.5, y: programaticallyAddedLabel.frame.origin.y + programaticallyAddedLabel.frame.height + 30, width: 175, height: 30)
+        let currencyTextField = UICurrencyTextField(frame: textFieldFrame)
+        currencyTextField.borderStyle = .roundedRect
+        
+        view.addSubview(currencyTextField)
     }
 }
