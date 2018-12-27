a60eefef108d55a9373c6d4befbcaad069a79a8b
Felipe Lefèvre Marino <felipemarino91@gmail.com>
2018-12-26 22:17:10 -0200

    UITextField extension

---
+++ UITextField.swift
@@ -0,0 +1,17 @@
+//
+//  UITextField.swift
+//  UICurrencyTextField
+//
+//  Created by Felipe Lefèvre Marino on 12/26/18.
+//
+
+import UIKit
+
+extension UITextField {
+    
+    func updateSelectedTextRange(offset: Int) {
+        if let updatedCursorPosition = position(from: endOfDocument, offset: offset) {
+            selectedTextRange = textRange(from: updatedCursorPosition, to: updatedCursorPosition)
+        }
+    }
+}
